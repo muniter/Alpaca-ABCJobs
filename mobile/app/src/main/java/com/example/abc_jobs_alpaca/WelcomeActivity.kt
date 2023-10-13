9f398c8eb4f2b1a8dfe883ea5f44580780026215
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2023-10-13 04:52:35 -0500

    feat: welcomeActivity

---
+++ mobile/app/src/main/java/com/example/abc_jobs_alpaca/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.abc_jobs_alpaca
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_welcome)
+
+    }
+}
