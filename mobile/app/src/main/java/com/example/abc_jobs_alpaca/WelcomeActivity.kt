6f94c689eb12ef36c26df94b111e9b9689b79bbd
Hector Tenezaca <htenezaca>
2023-10-17 01:04:44 -0500

    feat: language(not complete 2)

--- mobile/app/src/main/java/com/example/abc_jobs_alpaca/WelcomeActivity.kt
+++ mobile/app/src/main/java/com/example/abc_jobs_alpaca/WelcomeActivity.kt
@@ -22,6 +22,7 @@ import java.util.Locale
 class WelcomeActivity: AppCompatActivity(), WelcomeFragment.OnLanguageChangeListener {
     private lateinit var viewModel: CandidateRegisterModel
     private val toastMessage = MutableLiveData<String>()
+    private var currentLanguage: String = "es"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
@@ -38,23 +39,26 @@ class WelcomeActivity: AppCompatActivity(), WelcomeFragment.OnLanguageChangeList
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
+        var isRecreating = false  // Agrega esta variable de control
+
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                try {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                if (view != null) {
                     val selectedLanguage = languageOptions[position]
                     when (selectedLanguage) {
                         "Inglés" -> {
-                            setLocale(this@WelcomeActivity, "en")
-                            recreate()
+                            if (currentLanguage != "en") {
+                                setLocale("en")
+                                recreate()
+                            }
                         }
                         "Español" -> {
-                            setLocale(this@WelcomeActivity, "es")
-                            recreate()
+                            if (currentLanguage != "es") {
+                                setLocale("es")
+                                recreate()
+                            }
                         }
-
                     }
-                } catch (e: Exception) {
-                    e.printStackTrace()
                 }
             }
 
@@ -65,16 +69,33 @@ class WelcomeActivity: AppCompatActivity(), WelcomeFragment.OnLanguageChangeList
     }
 
     override fun onLanguageSelected(newLanguage: String) {
-        setLocale(this, newLanguage)
-
+        setLocale(newLanguage)
     }
 
-    fun setLocale(context: Context, languageCode: String) {
+    private fun setLocale(languageCode: String) {
         val locale = Locale(languageCode)
         Locale.setDefault(locale)
-        val configuration = Configuration(context.resources.configuration)
+        val resources = resources
+        val configuration = Configuration(resources.configuration)
         configuration.setLocale(locale)
-        context.resources.updateConfiguration(configuration, context.resources.displayMetrics)
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+
+        val sharedPreferences = getSharedPreferences("LanguagePrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("language", languageCode)
+        editor.apply()
+    }
+
+    // ...
+
+    override fun attachBaseContext(newBase: Context) {
+        // Obtiene la configuración del idioma almacenada en preferencias compartidas
+        val sharedPreferences = newBase.getSharedPreferences("LanguagePrefs", Context.MODE_PRIVATE)
+        val language = sharedPreferences.getString("language", "en") // "en" es el idioma predeterminado
+        val locale = Locale(language)
+        val configuration = Configuration(newBase.resources.configuration)
+        configuration.setLocale(locale)
+        super.attachBaseContext(newBase.createConfigurationContext(configuration))
     }
 
     private fun showToast(message: String) {
