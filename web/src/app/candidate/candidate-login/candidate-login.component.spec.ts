aa2bf2330dd8f0113c0b8336be30c9caa99f6846
Ronald Lugo <r.lugoq@uniandes.edu.co>
2023-10-22 21:52:04 +0000

    Configuraciones de usuario web + internacionalización + tests

--- web/src/app/candidate/candidate-login/candidate-login.component.spec.ts
+++ web/src/app/candidate/candidate-login/candidate-login.component.spec.ts
@@ -174,7 +174,7 @@ describe('CandidateLoginComponent', () => {
       "false"
     );
 
-    let candidateSignUpSpy = spyOn(candidateService, 'login').and.returnValue(of({ success: true }));
+    let candidateSignUpSpy = spyOn(candidateService, 'login').and.returnValue(of({ success: true, data: { token: 'fake-jwt-token' } }));
     let navigateSpy = spyOn(router, 'navigateByUrl').and.stub();
 
     component.candidateLogin(component.candidateLoginForm.value);
