8fea335d74f0aa73f63f21b38ad63cbb42193103
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2023-11-17 01:32:22 -0500

    interviews from candidate side

---
+++ mobile/app/src/main/java/com/example/abc_jobs_alpaca/view/InterviewsFragment.kt
@@ -0,0 +1,63 @@
+package com.example.abc_jobs_alpaca.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.abc_jobs_alpaca.R
+import com.example.abc_jobs_alpaca.adapter.InterviewItemRecyclerViewAdapter
+import com.example.abc_jobs_alpaca.view.placeholder.PlaceholderContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class InterviewsFragment : Fragment() {
+
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_interviews_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = InterviewItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            InterviewsFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
