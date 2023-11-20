04461759a175e61e7ab791f104f1fea0a4fcb2d9
Alejandro Santamaria <a.santamaria11@uniandes.edu.co>
2023-11-20 18:38:10 -0500

    fix tests

--- mobile/app/src/main/java/com/example/abc_jobs_alpaca/viewmodel/ExamTakeViewModel.kt
+++ mobile/app/src/main/java/com/example/abc_jobs_alpaca/viewmodel/ExamTakeViewModel.kt
@@ -3,18 +3,15 @@ package com.example.abc_jobs_alpaca.viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.abc_jobs_alpaca.model.models.Answer
 import com.example.abc_jobs_alpaca.model.models.Question
 import com.example.abc_jobs_alpaca.model.models.deserializerAnswers
 import com.example.abc_jobs_alpaca.model.repository.ABCJobsRepository
-import kotlinx.coroutines.launch
-import org.json.JSONObject
 
 
 class ExamTakeViewModel(
     private val abcJobsRepository: ABCJobsRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val tokenLiveData = MutableLiveData<String?>()
     val token = tokenLiveData
@@ -22,18 +19,48 @@ class ExamTakeViewModel(
     val answers = MutableLiveData<List<Answer>?>()
     val idResult = MutableLiveData<Int>()
 
-    fun onTokenUpdated(token: String?) {tokenLiveData.value = token}
+    fun onTokenUpdated(token: String?) {
+        tokenLiveData.value = token
+    }
 
     suspend fun postStartExam(idExam: Int) {
-        viewModelScope.launch {
-            try {
-                if (token != null) {
-                    abcJobsRepository.postExamStart(token.value!!, idExam)
+        try {
+            if (token != null) {
+                abcJobsRepository.postExamStart(token.value!!, idExam)
+                    .onSuccess { response ->
+                        if (response.success) {
+                            question.postValue(response.data.next_question)
+                            idResult.postValue(response.data.id_result)
+                            answers.postValue(response.data.next_question?.answers?.let {
+                                deserializerAnswers(
+                                    it
+                                )
+                            })
+                        } else {
+                            Log.d("ExamTakeViewModel", "postStartExam2: ${response.success}")
+
+                        }
+                    }
+                    .onFailure {
+                        Log.d("ExamTakeViewModel", "postStartExam3: ${it.message}")
+                    }
+            }
+        } catch (e: Exception) {
+            //TODO: Manejo de exceptions
+        }
+
+    }
+
+    suspend fun submitAnswer(idAnswer: Int) {
+        try {
+            if (token != null) {
+                val answer = answers.value?.find { it.id == idAnswer }
+                if (answer != null) {
+                    abcJobsRepository.postAnswerQuestion(token.value!!, idResult.value!!, answer)
                         .onSuccess { response ->
                             if (response.success) {
-                                question.postValue(response.data.next_question)
-                                idResult.postValue(response.data.id_result)
-                                answers.postValue(response.data.next_question?.answers?.let {
+                                question.value = (response.data.next_question)
+                                answers.value = (response.data.next_question?.answers?.let {
                                     deserializerAnswers(
                                         it
                                     )
@@ -47,48 +74,11 @@ class ExamTakeViewModel(
                             Log.d("ExamTakeViewModel", "postStartExam3: ${it.message}")
                         }
                 }
-            } catch (e: Exception) {
-                //TODO: Manejo de exceptions
             }
+        } catch (e: Exception) {
+            // TODO: Manejo de excepciones
         }
-    }
 
-
-    suspend fun submitAnswer(idAnswer: Int) {
-        viewModelScope.launch {
-            try {
-                if (token != null) {
-                    val answer = answers.value?.find { it.id == idAnswer }
-                    if (answer != null) {
-                        abcJobsRepository.postAnswerQuestion(token.value!!, idResult.value!!, answer)
-                            .onSuccess { response ->
-                                if (response.success) {
-                                    if(response.data.next_question == null){
-                                        question.postValue(null)
-                                        answers.postValue(null)
-                                    }
-                                    else{
-                                        question.postValue(response.data.next_question)
-                                        answers.postValue(response.data.next_question?.answers?.let {
-                                            deserializerAnswers(
-                                                it
-                                            )
-                                        })
-                                    }
-                                } else {
-                                    Log.d("ExamTakeViewModel", "postStartExam2: ${response.success}")
-
-                                }
-                            }
-                            .onFailure {
-                                Log.d("ExamTakeViewModel", "postStartExam3: ${it.message}")
-                            }
-                    }
-                }
-            } catch (e: Exception) {
-                // TODO: Manejo de excepciones
-            }
-        }
     }
 
 }
